1eeaab681a1f9f12a9f7592f0ed12a170c094f46
NN <asdzxc123fg@gmail.com>
2023-12-19 19:12:00 +0200

    Hm ex 3 done

--- my.py
+++ my.py
@@ -1,6 +1,4 @@
-
-salary = int(input("1"))
-bankloan = int(input("2"))
-utilities = int(input("3"))
-income = salary-bankloan-utilities
-print("Income: ", income)
+d1 = float(input("1"))
+d2 = float(input("2"))
+s= d1*d2*0.5
+print("Площа:",s)
